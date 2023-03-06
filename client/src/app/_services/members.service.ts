ad0116cff73adef4d46c1d7bc4c2717763d1b41b
Ray Finton <rfinton@outlook.com>
2023-03-06 08:43:22 -0600

    End of section 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -5,29 +5,28 @@ import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getMembers() {
-    if(this.members.length > 0)
-      return of(this.members);
+    if (this.members.length > 0) return of(this.members);
 
     return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      map(members => {
+      map((members) => {
         this.members = members;
         return members;
       })
-    )
+    );
   }
 
   getMember(username: string) {
-    const member = this.members.find(({userName}) => userName === username);
-    if(member) {
+    const member = this.members.find(({ userName }) => userName === username);
+    if (member) {
       return of(member);
     }
 
@@ -36,5 +35,13 @@ export class MembersService {
 
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member);
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 }
